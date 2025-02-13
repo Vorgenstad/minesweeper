74639dfd3d8dbd687dff3f1113b145b7e9f1321a
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2025-02-13 16:04:48 +0100

    Populated teh board grid manually

--- scenes/board/board.gd
+++ scenes/board/board.gd
@@ -3,6 +3,21 @@ extends CanvasLayer
 
 signal bomb_pressed
 
+var _grid: Array
+
+func _ready() -> void:
+	# TODO: Generate board and populate the grid
+	_grid = [[], [], [], [], [], []]
+
+	var cells := %GridContainer.get_children()
+
+	var index = 0
+
+	for row in _grid:
+		row.append(cells.slice(index, index + 10))
+
+		index += 10
+
 func _on_bomb_cell_pressed() -> void:
 	bomb_pressed.emit()
 
