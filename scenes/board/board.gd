322fe77629f07c94b170d27e771c19888bac1b24
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2025-02-14 13:45:06 +0100

    Added press propagation to empty cells that don't have any adjacent bomb

--- scenes/board/board.gd
+++ scenes/board/board.gd
@@ -5,21 +5,42 @@ signal bomb_pressed
 
 var _grid: Array
 
+const _DIRECTIONS = [-1, 0, 1]
+
 func _ready() -> void:
 	# TODO: Generate board and populate the grid
 	_grid = [[], [], [], [], [], []]
 
 	var cells := %GridContainer.get_children()
 
-	var index = 0
+	for x in 6:
+		for y in 10:
+			var cell: Cell = cells.pop_front()
 
-	for row in _grid:
-		row.append(cells.slice(index, index + 10))
+			if cell is EmptyCell:
+				cell.connect("pressed", _on_empty_cell_pressed.bind(x, y, cell.value))
 
-		index += 10
+			_grid[x].append(cell)
+
+func _propagate_press(x: int, y: int) -> void:
+	for i in _DIRECTIONS:
+		for j in _DIRECTIONS:
+			if _is_outside_grid(x + i, y + j):
+				continue
+			
+			var cell: Cell = _grid[x + i][y + j]
+
+			if cell is EmptyCell and !cell.is_pressed:
+				cell.press()
+
+func _is_outside_grid(x: int, y: int) -> bool:
+	return x < 0 or x >= _grid.size() or y < 0 or y >= _grid[0].size()
 
 func _on_bomb_cell_pressed() -> void:
 	bomb_pressed.emit()
 
-func _on_empty_cell_pressed(value: int) -> void:
-	print("Pressed cell with value: ", value)
+func _on_empty_cell_pressed(x: int, y: int, value: int) -> void:
+	if value != 0:
+		return
+	
+	_propagate_press(x, y)
--- scenes/cell/empty_cell.gd
+++ scenes/cell/empty_cell.gd
@@ -1,14 +1,21 @@
 class_name EmptyCell
 extends Cell
 
-signal pressed(value: int)
+signal pressed(x: int, y: int, value: int)
+
+var is_pressed := false
 
 @export var value := 0
 
 func _ready() -> void:
 	%Label.text = str(value) if value != 0 else ""
 
-func _on_button_pressed_left() -> void:
+func press() -> void:
+	is_pressed = true
+
 	%Button.visible = false
 
-	pressed.emit(value)
+	pressed.emit()
+
+func _on_button_pressed_left() -> void:
+	press()
