1ff905ddb8d2073d17eb0e7990cef8e6049d96a0
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2025-02-19 10:32:20 +0100

    Added random board generation

--- scenes/board/board.gd
+++ scenes/board/board.gd
@@ -3,24 +3,83 @@ extends CanvasLayer
 
 signal bomb_pressed
 
+@export var bomb_cell_scene: PackedScene
+@export var empty_cell_scene: PackedScene
+
 var _grid: Array
 
 const _DIRECTIONS = [-1, 0, 1]
 
-func _ready() -> void:
-	# TODO: Generate board and populate the grid
-	_grid = [[], [], [], [], [], []]
+func generate(rows: int, columns: int, bombs: int) -> void:
+	_generate_random_grid(rows, columns, bombs)
 
-	var cells := %GridContainer.get_children()
+	var grid_container = GridContainer.new()
 
-	for x in 6:
-		for y in 10:
-			var cell: Cell = cells.pop_front()
+	grid_container.columns = columns
+
+	_insert_grid_into_container(grid_container)
+
+	%PanelContainer.add_child(grid_container)
+
+func _generate_random_grid(rows: int, columns: int, bombs: int) -> void:
+	var flat_grid = []
+
+	for row in rows:
+		for column in columns:
+			if (bombs > 0):
+				var bomb_cell = _setup_bomb_cell()
+
+				flat_grid.append(bomb_cell)
+
+				bombs -= 1
+			else:
+				var empty_cell = empty_cell_scene.instantiate()
+
+				flat_grid.append(empty_cell)
+
+	flat_grid.shuffle()
+
+	var index = 0
+
+	for row in rows:
+		_grid.append([])
+
+		for column in columns:
+			_grid[row].append(flat_grid[index])
+			
+			index += 1
+
+func _setup_bomb_cell() -> BombCell:
+	var bomb_cell = bomb_cell_scene.instantiate()
+
+	bomb_cell.connect("pressed", _on_bomb_cell_pressed)
+
+	return bomb_cell
+
+func _insert_grid_into_container(grid_container: GridContainer) -> void:
+	for row in _grid.size():
+		for column in _grid[0].size():
+			var cell: Cell = _grid[row][column]
 
 			if cell is EmptyCell:
-				cell.connect("pressed", _on_empty_cell_pressed.bind(x, y, cell.value))
+				_setup_empty_cell(cell, row, column)
 
-			_grid[x].append(cell)
+			grid_container.add_child(cell)
+
+func _setup_empty_cell(empty_cell: EmptyCell, row: int, column: int) -> void:
+	var value := 0
+
+	for i in _DIRECTIONS:
+		for j in _DIRECTIONS:
+			if _is_outside_grid(row + i, column + j):
+				continue
+
+			if _grid[row + i][column + j] is BombCell:
+				value += 1
+	
+	empty_cell.value = value
+
+	empty_cell.connect("pressed", _on_empty_cell_pressed.bind(row, column, value))
 
 func _propagate_press(x: int, y: int) -> void:
 	for i in _DIRECTIONS:
--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,5 +1,16 @@
 class_name Game
 extends CanvasLayer
 
+@export var board_scene: PackedScene
+
+func _ready() -> void:
+	var board = board_scene.instantiate()
+
+	board.connect("bomb_pressed", _on_board_bomb_pressed)
+
+	board.generate(10, 10, 20)
+
+	add_child(board)
+
 func _on_board_bomb_pressed() -> void:
 	print("Game over!")
