87c45df97482e643bb32d0edfebda06d9eb7014f
Vorgenstad <rurulururl@hotmail.es>
2025-11-07 19:47:37 +0100

    Added "flagged" flag for cells that have a flag

--- scenes/cell/cell.gd
+++ scenes/cell/cell.gd
@@ -1,10 +1,17 @@
 class_name Cell
 extends PanelContainer
 
+var flagged := false
+
 func _on_button_pressed_right() -> void:
-	if %Button.disabled:
-		%Button.text = ""
-		%Button.disabled = false
-	else:
+	flagged = !flagged
+
+	_update_state()
+
+func _update_state() -> void:
+	if flagged:
 		%Button.text = "🚩"
 		%Button.disabled = true
+	else:
+		%Button.text = ""
+		%Button.disabled = false
